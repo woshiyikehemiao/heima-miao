be8ccd75efd75cb8b576524ecc8b30eafc984e77
崔苗苗 <1940803799@qq.com>
2019-09-03 22:57:46 +0800

    素材列表全部素材地数据加载

--- src/views/meterial/index.vue
+++ src/views/meterial/index.vue
@@ -1,13 +1,68 @@
 <template>
-  <div>meterial</div>
+<el-card class="card">
+  <bread-crumb slot="header">
+  <template slot="title">
+    素材管理
+  </template>
+  </bread-crumb>
+  <el-tabs v-model="activeName">
+    <el-tab-pane label="用户管理" name="all" class="tab">
+      <!-- 获取参数便利生成图片 -->
+      <el-card v-for="item in list" :key='item.id' class="box">
+        <img :src="item.url" alt="">
+      </el-card>
+    </el-tab-pane>
+    <el-tab-pane label="配置管理" name="collect">
+      <!-- 获取参数便利生成图片 -->
+
+    </el-tab-pane>
+  </el-tabs>
+</el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all',
+      list: []
+    }
+  },
+  methods: {
+    getMeterial () {
+      this.$axios({
+        url: '/user/images',
+        params: {
+          collect: false
+        }
+      }).then(res => {
+        console.log(res)
+        this.list = res.data.results
+      })
+    }
+  },
+  created () {
+    this.getMeterial()
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.card{
+  .tab{
+    display: flex;
+    flex-wrap: wrap;
+    .box{
+      // flex: 20%;
+      width:200px;
+      height: 200px;
+      margin: 0 10px;
+      img{
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
 
 </style>

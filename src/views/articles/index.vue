392c5bc1cb563a9c0db7109d2a36cb625c671d01
崔苗苗 <1940803799@qq.com>
2019-09-05 11:16:51 +0800

    内容列表-页面结构-请求数据

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -6,11 +6,11 @@
     <el-form class="form">
       <el-form-item label="文章状态：">
         <el-radio-group v-model="radio">
-          <el-radio :label="1">全部</el-radio>
-          <el-radio :label="2">草稿</el-radio>
-          <el-radio :label="3">待审核</el-radio>
-          <el-radio :label="4">审核通过</el-radio>
-          <el-radio :label="5">审核失败</el-radio>
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核通过</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item label="频道列表：">
@@ -29,13 +29,13 @@
     </el-form>
     <div class="article-number">共找到xxx条符合条件的内容</div>
     <div class="article-list">
-    <div class="row" v-for="(item,index) in list" :key='index'>
+    <div class="row" v-for="item in articleList" :key='item.id.toString()'>
         <div class="left">
-            <img src="../../assets/img/avatar.jpg" alt="">
+            <img :src="item.cover.images[0]?item.cover.images[0]:srcImg" alt="">
             <div class="content">
-                <span class="title">这是标题</span>
-                <span class="status">已发表</span>
-                <span class="time">123456</span>
+                <span class="title">{{item.title}}</span>
+                <span class="status">{{item.status}}</span>
+                <span class="time">{{item.pubdate}}</span>
             </div>
         </div>
         <div class="right">
@@ -54,8 +54,23 @@ export default {
       radio: 1,
       articleSelect: '',
       articleDate: '',
-      list: [1, 2, 3, 4, 5, 6]
+      list: [1, 2, 3, 4, 5, 6],
+      articleList: [],
+      srcImg: require('../../assets/img/avatar.jpg')
     }
+  },
+  methods: {
+    getArticles () {
+      this.$axios({
+        url: '/articles'
+      }).then(res => {
+        console.log(res)
+        this.articleList = res.data.results
+      })
+    }
+  },
+  created () {
+    this.getArticles()
   }
 }
 </script>
